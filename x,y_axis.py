759767d53776e7dc83247ba2309d940d5d6b7508
Chanyeol Ryu <62700997+chanyeol-ryu@users.noreply.github.com>
2023-03-15 22:53:37 +0900

    Update x,y_axis.py

--- x,y_axis.py
+++ x,y_axis.py
@@ -6,5 +6,5 @@ TM = 0
 while TM == 0:
     if keyboard.is_pressed('F4'):
         x, y = pg.position()           # 포인터 위치 찾기 (x,y분리)
-        alertbox = pg.alert(title='완료', text='x좌표:{} y좌표:{}'.format(x,y), button='OK')
+        alertbox = pg.alert(title='좌표', text='x좌표:{} y좌표:{}'.format(x,y), button='OK')
         break
